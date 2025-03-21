7d8743254d3cdc4e760c4761fe2e1ffee2528a55
Jedsada Meesuk <6733037621@student.chula.ac.th>
2025-03-22 04:50:21 +0700

    edit styles of about us

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,12 +6,12 @@ export default function About() {
                 <div className="text-3xl">About Us</div>
                 <div className="text-xl mt-[5px]">Powered By Dekpun</div>
                 </div>
-            <div className="m-5 flex flex-row justify-around space-x-5 rounded-lg overflow-hidden">
+            <div className="m-5 flex flex-row justify-around space-x-1  ">
                 <Member img={'/img/member/TJ.jpg'} sid={"6733037621"} name={"Jedsada Meesuk"}/>
-                <Member img={'/img/member/Pun.jpg'} sid={"6733037621"} name={"Jedsada Meesuk"}/>
-                <Member img={'/img/member/Boom.jpg'} sid={"6733037621"} name={"Jedsada Meesuk"}/>
+                <Member img={'/img/member/Pun.jpg'} sid={"6733037621"} name={"Patthadon Phengpinij"}/>
+                <Member img={'/img/member/Boom.jpg'} sid={"6733169821"} name={"Patcharapon Srisuwan"}/>
             </div>
-
         </main>
     );
 }
+
--- src/components/Member.tsx
+++ src/components/Member.tsx
@@ -1,11 +1,24 @@
+'use client'
 import Image from "next/image";
 export default function Member({img,sid,name}:{img:string,sid:string,name:string}){
+    function onCardMouseAction(event: React.SyntheticEvent) {
+            if(event.type == 'mouseover') {
+                event.currentTarget.classList.remove('shadow-md', 'scale-100')
+                event.currentTarget.classList.add('shadow-xl', 'scale-105')
+            } else {
+                event.currentTarget.classList.remove('shadow-xl', 'scale-105')
+                event.currentTarget.classList.add('shadow-md', 'scale-100')
+            }
+        }
     return(
-        <div className="w-[360px] h-[440px] rounded-lg shadow-lg transition-transform duration-500 ease-in-out scale-100 hover:bg-gray-400 hover:text-white">
-            <div className="rounded-lg overflow-hidden w-full h-[80%] relative">
+        <div className="w-[360px] h-[400px] rounded-lg bg-sky-50 shadow-md transition-transform duration-500 ease-in-out scale-100 hover:bg-sky-200 hover:text-[#01169C]"
+        onMouseOver={(e)=>onCardMouseAction(e)}
+        onMouseOut= {(e)=>onCardMouseAction(e)}>
+            <div className="block h-[5%]"></div>
+            <div className="rounded-lg overflow-hidden w-[90%] h-[70%] relative ml-[5%]">
                 <Image src={img} alt="Member Picture" fill={true} objectFit='cover'/>
             </div>
-            <div className="text-md font-verdana ">
+            <div className="text-md font-verdana mt-4 font-bold ml-[8%]">
                 <div>SID : {sid}</div>
                 <div>Name : {name}</div>
             </div>
