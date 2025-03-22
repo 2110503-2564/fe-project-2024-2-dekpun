bfcd006cc6a0a7acf259fc883d6f7d2da3487603
Patthadon Phengpinij <patthadon.1807@gmail.com>
2025-03-22 13:41:37 +0700

    add subpage for dentist

--- src/components/TopMenuItem.tsx
+++ src/components/TopMenuItem.tsx
@@ -1,10 +1,49 @@
+"use client"
 import Link from 'next/link';
+import { useState } from 'react';
 
-export default function TopMenuItem( { title, pageRef } : { title:string, pageRef:string } ) {
+export default function TopMenuItem( { title, pageRef, subPage } : { title:string, pageRef:string, subPage?:string[] } ) {
+
+    const [isHovered, setIsHovered] = useState(false);
+
     return(
-        <Link href={pageRef} className="w-[150px] text-center mt-auto mb-auto 
-          font-verdana text-md text-[#01169C] font-bold">
-            {title}
-        </Link>
+        <div
+            className="relative text-center flex px-5"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+
+            {
+                subPage ?
+                    <div className="block mt-auto mb-auto font-verdana text-md text-[#01169C] font-bold">
+                        { title }
+                    </div>
+                :
+                    <Link href={pageRef} className="block mt-auto mb-auto font-verdana text-md text-[#01169C] font-bold">
+                        { title }
+                    </Link>
+            }
+
+            {
+                subPage && isHovered ?
+                    (
+                        <div className="absolute left-0 top-full w-full bg-white shadow-lg border border-gray-300 rounded-md">
+                            {
+                                subPage.map((sub, index) => (
+                                    <Link 
+                                        key={index} 
+                                        href={`${pageRef}/${sub}`} 
+                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200"
+                                    >
+                                        {sub}
+                                    </Link>
+                                ))
+                            }
+                        </div>
+                    )
+                :
+                    <></>
+            }
+        </div>
     );
 };
