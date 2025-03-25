70b1083c76a654e345a63db498bef8f12117dba4
Jedsada Meesuk <6733037621@student.chula.ac.th>
2025-03-25 15:03:10 +0700

    modify style of log in btn

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -54,8 +54,8 @@ export default function TopMenu( {session, token} : {session?: any, token?: stri
                         </>
                     ) : (
                         <Link href="/api/auth/signin" className='flex items-center'>
-                            <div className='flex items-center h-[70%] px-4 py-2 bg-cyan-600 text-white rounded-lg hover:bg-cyan-700 transition duration-200 ease-in-out cursor-pointer ml-5'>
-                                <span className='text-sm font-medium'>Sign-In</span>
+                            <div className='flex items-center h-[70%] px-4 py-2 bg-[#0218A0] text-white rounded-lg hover:bg-[#021382] transition duration-200 ease-in-out cursor-pointer ml-5'>
+                                <span className='text-sm font-medium'>Log-In</span>
                             </div>    
                         </Link>
                     )
