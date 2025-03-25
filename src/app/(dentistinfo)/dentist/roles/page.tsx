438ace86b30667113a94d64871fb25745055c0a0
Patcharapon Srisuwan <rirhcceez@gmail.com>
2025-03-25 20:46:48 +0700

    add loading style

--- src/app/(dentistinfo)/dentist/roles/page.tsx
+++ src/app/(dentistinfo)/dentist/roles/page.tsx
@@ -1,15 +1,35 @@
-"use client"
+"use client";
 import RoleCatalog from "@/components/RoleCatalog";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
+import NProgress from "nprogress";
 import { LinearProgress } from "@mui/material";
+import 'nprogress/nprogress.css';
+import Image from "next/image";
 
 export default function DentistRoles() {
 
-    return(
-        <main className="text-center p-5">
-            <Suspense fallback={ <p>Loading ... <LinearProgress/></p> }>
-                <RoleCatalog />
-            </Suspense>
-        </main>
-    );
-};
+  useEffect(() => {
+    // Start progress bar when page starts loading
+    NProgress.start();
+
+    // Stop progress bar when page finishes loading
+    return () => {
+      NProgress.done();
+    };
+  }, []);
+
+  return (
+    <main className="text-center p-5">
+      <Suspense fallback={
+        <div className="flex items-center justify-center space-x-3 h-[70%] mt-[250px]">
+          <div className="animate-spin rounded-full border-t-2 border-green-500 w-10 h-10">
+            <Image src="/img/tooth.png" alt="tooth" width={600} height={600}/>   
+          </div> {/* Tooth icon spinner */}
+          <p className="text-3xl">Loading...</p>
+        </div>
+      }>
+        <RoleCatalog />
+      </Suspense>
+    </main>
+  );
+}
